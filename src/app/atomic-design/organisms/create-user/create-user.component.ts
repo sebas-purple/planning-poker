2b9e8f2c8b1df95de8982d9b5cf8371b5092e37d
sebudea <sebastian.aristizabalc@udea.edu.co>
2025-09-28 14:49:49 -0500

    feat: agrega organismo create user

---
+++ src/app/atomic-design/organisms/create-user/create-user.component.ts
@@ -0,0 +1,64 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from "../../atoms/container/container.component";
+import { InputLabelComponent } from "../../molecules/input-label/input-label.component";
+import { CheckboxLabelComponent } from "../../molecules/checkbox-label/checkbox-label.component";
+import { TypographyComponent } from "../../atoms/typography/typography.component";
+import { ButtonComponent } from "../../atoms/button/button.component";
+import { LabelType } from '../../atoms/label/label.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { nameValidator } from 'src/app/shared/validators/name-validator';
+import { ViewMode } from 'src/app/core/enums/view-mode.enum';
+
+@Component({
+  selector: 'o-create-user',
+  standalone: true,
+  imports: [CommonModule, ContainerComponent, InputLabelComponent, CheckboxLabelComponent, TypographyComponent, ButtonComponent, ReactiveFormsModule],
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.scss']
+})
+export class CreateUserComponent {
+  @Input() textLabel = "Tu nombre";
+  @Input() textLabeljugador = "Jugador";
+  @Input() textLabelespectador = "Espectador";
+  @Input() textButton = "Continuar";
+  @Input() labelType: LabelType = "small";
+  @Input() jugador: ViewMode = ViewMode.jugador;
+  @Input() espectador: ViewMode = ViewMode.espectador;
+  
+  @Output() formSubmit = new EventEmitter<{name: string, viewMode: ViewMode}>();
+
+  messageError = "";
+
+  gameRoomForm = new FormGroup({
+    name: new FormControl("", [Validators.required, nameValidator()]),
+    selectedOption: new FormControl("", [Validators.required])
+  });
+
+  handleSubmit() {
+    if (this.gameRoomForm.valid) {
+      const name = this.gameRoomForm.value.name?.trim() || '';
+      const viewMode = this.gameRoomForm.value.selectedOption?.trim() as ViewMode;
+      
+      this.formSubmit.emit({ name, viewMode });
+      this.gameRoomForm.reset();
+      this.messageError = "";
+    } else {
+      const errors = this.gameRoomForm.controls.name.errors;
+      if (errors) {
+        if (errors['specialChars'] || errors['required']) {
+          this.messageError = errors['message'];
+          return;
+        } else {
+          const errorKey = Object.keys(errors)[0];
+          this.messageError = errors[errorKey].message;
+          return;
+        }
+      }
+      
+      if (!this.gameRoomForm.value.selectedOption) {
+        this.messageError = "Debes seleccionar un modo de visualización";
+      }
+    }
+  }
+}
--- src/app/pages/game-room/game-room.component.ts
+++ src/app/pages/game-room/game-room.component.ts
@@ -1,24 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { LabelType } from "src/app/atomic-design/atoms/label/label.component";
-import { nameValidator } from 'src/app/shared/validators/name-validator';
-import { ButtonComponent } from "src/app/atomic-design/atoms/button/button.component";
 import { ViewMode } from 'src/app/core/enums/view-mode.enum';
-import { ContainerComponent } from "src/app/atomic-design/atoms/container/container.component";
-import { InputLabelComponent } from "src/app/atomic-design/molecules/input-label/input-label.component";
-import { CheckboxLabelComponent } from "src/app/atomic-design/molecules/checkbox-label/checkbox-label.component";
 import { UserService } from 'src/app/services/user.service';
-import { TypographyComponent } from "src/app/atomic-design/atoms/typography/typography.component";
 import { GameService } from 'src/app/services/game.service';
-import { CardComponent } from "src/app/atomic-design/atoms/card/card.component";
-import { CardLabelComponent } from "src/app/atomic-design/molecules/card-label/card-label.component";
-import { TableComponent } from "src/app/atomic-design/atoms/table/table.component";
+import { CreateUserComponent } from 'src/app/atomic-design/organisms/create-user/create-user.component';
+import { LabelType } from 'src/app/atomic-design/atoms/label/label.component';
 
 @Component({
   selector: 'app-game-room',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ButtonComponent, ContainerComponent, InputLabelComponent, CheckboxLabelComponent, TypographyComponent, CardComponent, CardLabelComponent, TableComponent],
+  imports: [CommonModule, CreateUserComponent],
   templateUrl: './game-room.component.html',
   styleUrls: ['./game-room.component.scss']
 })
@@ -26,63 +17,24 @@ export class GameRoomComponent {
   private readonly userService: UserService = inject(UserService);
   private readonly gameService: GameService = inject(GameService);
 
+  // para manejar la creacion del usuario
   textLabel = "Tu nombre";
-  textButton = "Continuar";
-
   textLabeljugador = "Jugador";
   textLabelespectador = "Espectador";
-
+  textButton = "Continuar";
   labelType: LabelType = "small";
-
   jugador: ViewMode = ViewMode.jugador;
   espectador: ViewMode = ViewMode.espectador;
 
-  gameRoomForm = new FormGroup({
-    name: new FormControl("", [Validators.required, nameValidator()]),
-    selectedOption: new FormControl("", [Validators.required])
-  })
+  handleCreateUser(event: {name: string, viewMode: ViewMode}) {
+    try {
+      const newUser = this.userService.createUser(event.name, event.viewMode);
+      this.gameService.setGameOwner(newUser.name);
 
-  messageError = "";
-
-  handleSubmit() {
-    if (this.gameRoomForm.valid) {
-      const name = this.gameRoomForm.value.name?.trim() || '';
-      const viewMode = this.gameRoomForm.value.selectedOption?.trim() as ViewMode;
-      
-      try {
-        const newUser = this.userService.createUser(name, viewMode);
-        this.gameService.setGameOwner(newUser.name);
-
-        console.log('Usuario creado exitosamente:', newUser);
-        console.log('Partida creada exitosamente:', this.gameService.getCurrentGame());
-
-        this.gameRoomForm.reset();
-        this.messageError = "";
-        // Aquí podríamos agregar navegación a la siguiente pantalla o mostrar un mensaje de éxito
-      } catch (error) {
-        this.messageError = "Error al crear el usuario";
-        console.error('Error al crear usuario:', error);
-      }
-    } else {
-      const errors = this.gameRoomForm.controls.name.errors;
-      if (errors) {
-        // Para el caso especial de specialChars y required que tiene el mensaje directamente
-        if (errors['specialChars'] || errors['required']) {
-          this.messageError = errors['message'];
-          return;
-        } else {
-          const errorKey = Object.keys(errors)[0];
-          this.messageError = errors[errorKey].message;
-          return;
-        }
-      }
-      
-      // Si no hay errores de nombre pero el formulario es inválido, probablemente es el modo de visualización
-      if (!this.gameRoomForm.value.selectedOption) {
-        this.messageError = "Debes seleccionar un modo de visualización";
-      }
+      console.log('Usuario creado exitosamente:', newUser);
+      console.log('Partida creada exitosamente:', this.gameService.getCurrentGame());
+    } catch (error) {
+      console.error('Error al crear usuario:', error);
     }
   }
-
-
 }
