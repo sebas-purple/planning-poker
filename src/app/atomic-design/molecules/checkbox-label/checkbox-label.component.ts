37fc0118c34a5ee7ca7770b1f99f27b051f69403
sebudea <sebastian.aristizabalc@udea.edu.co>
2025-10-01 16:11:20 -0500

    refactor: mejora molecula checkbox label

--- src/app/atomic-design/molecules/checkbox-label/checkbox-label.component.ts
+++ src/app/atomic-design/molecules/checkbox-label/checkbox-label.component.ts
@@ -1,13 +1,12 @@
 import { Component, forwardRef, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LabelComponent, LabelType } from "../../atoms/label/label.component";
 import { CheckboxComponent } from "../../atoms/checkbox/checkbox.component";
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'm-checkbox-label',
   standalone: true,
-  imports: [CommonModule, LabelComponent, CheckboxComponent],
+  imports: [CommonModule, CheckboxComponent],
   templateUrl: './checkbox-label.component.html',
   styleUrls: ['./checkbox-label.component.scss'],
   providers: [
@@ -22,7 +21,6 @@ export class CheckboxLabelComponent implements ControlValueAccessor {
   @Input({required: true}) text!: string;
   @Input({required: true}) value!: any;
   @Input({required: true}) formControl!: FormControl;
-  @Input({required: true}) type!: LabelType;
   onChange: any = () => {};
   onTouched: any = () => {};
 
