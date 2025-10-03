c99027d799f3010d933aba324426b42207f8266b
Sebastian Aristizabal Castañeda <sebastian.aristizabal@pragma.com.co>
2025-10-03 15:44:44 -0500

    test: agrega test a atomo card

--- src/app/atomic-design/atoms/card/card.component.spec.ts
+++ src/app/atomic-design/atoms/card/card.component.spec.ts
@@ -8,7 +8,7 @@ describe('CardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [CardComponent]
+      imports: [CardComponent],
     });
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
@@ -17,5 +17,43 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render card with text', () => {
+    component.text = 'Test Card';
+    fixture.detectChanges();
+    const card = fixture.nativeElement.querySelector('div');
+    expect(card.textContent).toContain('Test Card');
+  });
+
+  it('should emit click event when clicked, only if type is choice', () => {
+    jest.spyOn(component.cardClick, 'emit');
+    const card = fixture.nativeElement.querySelector('div');
+    component.type = 'choice';
+    card.click();
+    expect(component.cardClick.emit).toHaveBeenCalled();
+  });
+
+  it('should not emit click event when disabled', () => {
+    jest.spyOn(component.cardClick, 'emit');
+    const card = fixture.nativeElement.querySelector('div');
+    card.click();
+    expect(component.cardClick.emit).not.toHaveBeenCalled();
+  });
+
+  it('should not emit click event when type is not choice', () => {
+    jest.spyOn(component.cardClick, 'emit');
+    const card = fixture.nativeElement.querySelector('div');
+    component.type = 'player';
+    card.click();
+    expect(component.cardClick.emit).not.toHaveBeenCalled();
+
+    component.type = 'viewer';
+    card.click();
+    expect(component.cardClick.emit).not.toHaveBeenCalled();
+
+    component.type = 'profile';
+    card.click();
+    expect(component.cardClick.emit).not.toHaveBeenCalled();
   });
 });
