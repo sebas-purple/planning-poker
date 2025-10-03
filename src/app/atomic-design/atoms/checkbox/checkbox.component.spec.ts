45612aa82077be5d13998c4fa8a1cd6012e99634
Sebastian Aristizabal Castañeda <sebastian.aristizabal@pragma.com.co>
2025-10-03 15:50:06 -0500

    test: agrega test a atomo checkbox

--- src/app/atomic-design/atoms/checkbox/checkbox.component.spec.ts
+++ src/app/atomic-design/atoms/checkbox/checkbox.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckboxComponent } from './checkbox.component';
+import { FormControl } from '@angular/forms';
 
 describe('CheckboxComponent', () => {
   let component: CheckboxComponent;
@@ -8,14 +9,23 @@ describe('CheckboxComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [CheckboxComponent]
+      imports: [CheckboxComponent],
     });
     fixture = TestBed.createComponent(CheckboxComponent);
     component = fixture.componentInstance;
+    component.formControl = new FormControl('');
+    component.value = 'Test Checkbox';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit change event when checkbox is changed', () => {
+    jest.spyOn(component.formControl, 'setValue');
+    const checkbox = fixture.nativeElement.querySelector('input');
+    checkbox.click();
+    expect(component.formControl.setValue).toHaveBeenCalled();
   });
 });
