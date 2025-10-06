9ab1aacc5c58004bfd7ee1a98e8288245f740aba
sebudea <sebastian.aristizabalc@udea.edu.co>
2025-10-06 11:24:28 -0500

    test: agrega pruebas a page splash screen

--- src/app/pages/splash-screen/splash-screen.component.spec.ts
+++ src/app/pages/splash-screen/splash-screen.component.spec.ts
@@ -5,17 +5,34 @@ import { SplashScreenComponent } from './splash-screen.component';
 describe('SplashScreenComponent', () => {
   let component: SplashScreenComponent;
   let fixture: ComponentFixture<SplashScreenComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [SplashScreenComponent]
+      imports: [SplashScreenComponent],
     });
     fixture = TestBed.createComponent(SplashScreenComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render splash screen with image', () => {
+    fixture.detectChanges();
+    const image = compiled.querySelector('img');
+    expect(image).toBeTruthy();
+  });
+
+  it('should render splash screen with correct images', () => {
+    fixture.detectChanges();
+    const image = compiled.querySelectorAll('img');
+    const image1 = image[0];
+    const image2 = image[1];
+    expect(image1?.src).toContain('assets/logo/isotipo_blanco.svg');
+    expect(image2?.src).toContain('assets/logo/logo_blanco.svg');
   });
 });
--- src/app/pages/splash-screen/splash-screen.component.ts
+++ src/app/pages/splash-screen/splash-screen.component.ts
@@ -8,18 +8,18 @@ import { ImageSize } from 'src/app/shared/types/_types';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './splash-screen.component.html',
-  styleUrls: ['./splash-screen.component.scss']
+  styleUrls: ['./splash-screen.component.scss'],
 })
 export class SplashScreenComponent {
-  srcImage = "assets/logo/isotipo_blanco.svg";
-  alt = "isotipo";
-  sizeImage: ImageSize = "small";
+  srcImage = 'assets/logo/isotipo_blanco.svg';
+  alt = 'isotipo';
+  sizeImage: ImageSize = 'small';
 
-  srcImagen2 = "assets/logo/logo_blanco.svg";
-  alt2 = "logo";
-  sizeImagen2: ImageSize = "large";
+  srcImagen2 = 'assets/logo/logo_blanco.svg';
+  alt2 = 'logo';
+  sizeImagen2: ImageSize = 'large';
 
-  private readonly router: Router = inject(Router);
+  readonly router: Router = inject(Router);
 
   ngOnInit(): void {
     setTimeout(() => {
