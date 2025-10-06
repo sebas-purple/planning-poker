2c4e984abc1a78239cd4f1ede56a0102216958ff
Sebastian Aristizabal Castañeda <sebastian.aristizabal@pragma.com.co>
2025-10-06 09:21:34 -0500

    test: agrega test a molecula card label

--- src/app/atomic-design/molecules/card-label/card-label.component.spec.ts
+++ src/app/atomic-design/molecules/card-label/card-label.component.spec.ts
@@ -8,14 +8,29 @@ describe('CardLabelComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [CardLabelComponent]
+      imports: [CardLabelComponent],
     });
     fixture = TestBed.createComponent(CardLabelComponent);
     component = fixture.componentInstance;
+    component.cardtype = 'choice';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render card label with text', () => {
+    component.text = 'Test Text';
+    fixture.detectChanges();
+    const cardLabel = fixture.nativeElement.querySelector('m-card-label');
+    expect(cardLabel.textContent).toContain('Test Text');
+  });
+
+  it('should render card label with type', () => {
+    component.cardtype = 'choice';
+    fixture.detectChanges();
+    const cardLabel = fixture.nativeElement.querySelector('m-card-label');
+    expect(cardLabel.classList.contains('card-label--choice')).toBe(true);
   });
 });
