33515c1cd81c00c4100397ac14cc9ba411d04fed
sebudea <sebastian.aristizabalc@udea.edu.co>
2025-10-07 09:47:31 -0500

    test: agrega pruebas a pagina game room header

--- src/app/pages/game-room/game-room-header/game-room-header.component.spec.ts
+++ src/app/pages/game-room/game-room-header/game-room-header.component.spec.ts
@@ -10,32 +10,36 @@ import { GameService } from '../../../services/game.service';
 describe('GameRoomHeaderComponent', () => {
   let component: GameRoomHeaderComponent;
   let fixture: ComponentFixture<GameRoomHeaderComponent>;
+  let userService: UserService;
+  let gameService: GameService;
+
+  // Mocks
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [GameRoomHeaderComponent],
+      providers: [UserService, GameService],
     });
     fixture = TestBed.createComponent(GameRoomHeaderComponent);
     component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    gameService = TestBed.inject(GameService);
 
-    // mock the user service
-    const userService = TestBed.inject(UserService);
-    Object.defineProperty(userService, 'getCurrentUser', {
-      get: jest.fn().mockReturnValue({
-        id: '1',
-        name: 'John Doe',
-        rol: UserRole.propietario,
-        viewMode: 'jugador',
-      }),
+    component.currentGame = {
+      id: '1',
+      name: 'Party Poker',
+      createdAt: new Date(),
+      players: [],
+      maxPlayers: 10,
+    };
+
+    jest.spyOn(userService, 'getCurrentUser', 'get').mockReturnValue({
+      id: '1',
+      name: 'John Doe',
+      rol: UserRole.propietario,
+      viewMode: ViewMode.jugador,
     });
 
-    // mock textCard
-    Object.defineProperty(userService, 'textCard', {
-      get: jest.fn().mockReturnValue('JO'),
-    });
-
-    // mock the game service
-    const gameService = TestBed.inject(GameService);
     jest.spyOn(gameService, 'isAdmin').mockReturnValue(true);
 
     fixture.detectChanges();
@@ -45,42 +49,44 @@ describe('GameRoomHeaderComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // testear que se muestre el logo
   it('should show the logo', () => {
     const logo = fixture.debugElement.query(By.css('img'));
     expect(logo).toBeTruthy();
     expect(logo.properties['src']).toContain('assets/logo/isotipo_blanco.svg');
   });
 
-  // testear que se muestre el texto del titulo
   it('should show the title text', () => {
     const title = fixture.debugElement.query(By.css('a-typography'));
     expect(title).toBeTruthy();
-    expect(title.properties['text']).toBe('Planning Poker');
+    expect(title.componentInstance.text).toContain('Party Poker');
   });
 
-  // testear que se muestre el texto de la card
   it('should show the card text', () => {
     const card = fixture.debugElement.query(By.css('a-card'));
     expect(card).toBeTruthy();
-    expect(card.properties['text']).toBe('John Doe');
+    expect(card.componentInstance.text).toBe('JO');
   });
 
-  // testear que se muestre el toggle
   it('should show the toggle', () => {
     const toggle = fixture.debugElement.query(By.css('a-toggle'));
     expect(toggle).toBeTruthy();
-    expect(toggle.properties['options']).toEqual([
+    expect(toggle.componentInstance.options).toEqual([
       ViewMode.jugador,
       ViewMode.espectador,
     ]);
-    expect(toggle.properties['selectedValue']).toBe(ViewMode.jugador);
+    expect(toggle.componentInstance.selectedValue).toBe(ViewMode.jugador);
   });
 
-  // testear que se muestre el boton de invitar jugadores
   it('should show the invite players button', () => {
     const button = fixture.debugElement.query(By.css('a-button'));
     expect(button).toBeTruthy();
-    expect(button.properties['text']).toBe('Invitar jugadores');
+    expect(button.componentInstance.text).toBe('Invitar jugadores');
+  });
+
+  it('should show the invite players dialog', () => {
+    const button = fixture.debugElement.query(By.css('a-button'));
+    button.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(component.showDialog).toBe(true);
   });
 });
