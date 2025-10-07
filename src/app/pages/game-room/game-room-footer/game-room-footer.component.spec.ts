8d5d4ba5512fe9530c30616f47e92c56830f330e
sebudea <sebastian.aristizabalc@udea.edu.co>
2025-10-07 09:01:37 -0500

    test: refactoriza las pruebas de page game room footer

--- src/app/pages/game-room/game-room-footer/game-room-footer.component.spec.ts
+++ src/app/pages/game-room/game-room-footer/game-room-footer.component.spec.ts
@@ -5,6 +5,7 @@ import { UserService } from '../../../services/user.service';
 import { UserRole } from '../../../core/enums/user-role.enum';
 import { GameService } from '../../../services/game.service';
 import { By } from '@angular/platform-browser';
+import { ViewMode } from '../../../core/enums/view-mode.enum';
 
 describe('GameRoomFooterComponent', () => {
   let component: GameRoomFooterComponent;
@@ -21,71 +22,114 @@ describe('GameRoomFooterComponent', () => {
     component = fixture.componentInstance;
     userService = TestBed.inject(UserService);
     gameService = TestBed.inject(GameService);
-
-    const spyUserService = jest.spyOn(userService, 'getCurrentUser', 'get');
-    const spyGameService = jest.spyOn(gameService, 'isAdmin');
-
-    // // mock the user service
-    // Object.defineProperty(userService, 'getCurrentUser', {
-    //   get: jest.fn().mockReturnValue({
-    //     id: '1',
-    //     name: 'John Doe',
-    //     rol: UserRole.propietario,
-    //     viewMode: 'jugador',
-    //   }),
-    // });
-
-    // // mock the game service
-    // jest.spyOn(gameService, 'isAdmin').mockReturnValue(true);
-    // // mock isRevealed
-    // Object.defineProperty(gameService, 'getIsRevealed', {
-    //   get: jest.fn().mockReturnValue(true),
-    // });
-
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should show the scoring mode selector if the user is admin', () => {
-    const selector = fixture.debugElement.query(By.css('a-selector'));
-    expect(selector).toBeTruthy();
-    expect(component.isAdmin).toBe(true);
+  describe('When user is admin (propietario)', () => {
+    beforeEach(() => {
+      jest.spyOn(gameService, 'isAdmin').mockReturnValue(true);
+      fixture.detectChanges();
+    });
+
+    it('should show the scoring mode selector', () => {
+      const selector = fixture.debugElement.query(By.css('a-selector'));
+      expect(component.isAdmin).toBe(true);
+      expect(selector).toBeTruthy();
+    });
   });
 
-  // ESTAS PRUEBAS PASAN DEPENDIENDO DEL ESTADO DE LA SESION
+  describe('When user is not admin', () => {
+    beforeEach(() => {
+      jest.spyOn(gameService, 'isAdmin').mockReturnValue(false);
+      fixture.detectChanges();
+    });
 
-  // it('should not show the scoring mode selector if the user is not admin', () => {
-  //   const selector = fixture.debugElement.query(By.css('a-selector'));
-  //   expect(selector).toBeNull();
-  //   expect(component.isAdmin).toBe(false);
-  // });
-
-  // it('should show the cards if the user has player view mode', () => {
-  //   const cards = fixture.debugElement.query(By.css('a-card'));
-  //   expect(component.userService.getCurrentUser?.viewMode).toBe('jugador');
-  //   expect(cards).toBeTruthy();
-  // });
-
-  // it('should not show the cards if the user has spectator view mode', () => {
-  //   const cards = fixture.debugElement.query(By.css('a-card'));
-  //   expect(component.userService.getCurrentUser?.viewMode).toBe('espectador');
-  //   expect(cards).toBeNull();
-  // });
-
-  // testear que se muestren las estadisticas cuando se revelan las cartas
-  it('should show the statistics when the cards are revealed', () => {
-    const statistics = fixture.debugElement.query(By.css('.statistics'));
-    expect(component.isRevealed).toBe(true);
-    expect(statistics).toBeTruthy();
+    it('should not show the scoring mode selector', () => {
+      const selector = fixture.debugElement.query(By.css('a-selector'));
+      expect(component.isAdmin).toBe(false);
+      expect(selector).toBeNull();
+    });
   });
 
-  // testear que se muestre el promedio cuando se revelan las cartas
-  it('should show the average when the cards are revealed', () => {
-    const average = fixture.debugElement.query(By.css('.average'));
-    expect(component.isRevealed).toBe(true);
-    expect(average).toBeTruthy();
+  describe('When user has player view mode', () => {
+    beforeEach(() => {
+      jest.spyOn(userService, 'getCurrentUser', 'get').mockReturnValue({
+        id: '1',
+        name: 'John Doe',
+        rol: UserRole.propietario,
+        viewMode: ViewMode.jugador,
+      });
+      fixture.detectChanges();
+    });
+
+    it('should show the cards', () => {
+      expect(component.userService.getCurrentUser?.viewMode).toBe(
+        ViewMode.jugador
+      );
+      const cards = fixture.debugElement.query(By.css('a-card'));
+      expect(cards).toBeTruthy();
+    });
+  });
+
+  describe('When user has spectator view mode', () => {
+    beforeEach(() => {
+      jest.spyOn(userService, 'getCurrentUser', 'get').mockReturnValue({
+        id: '2',
+        name: 'Jane Doe',
+        rol: UserRole.propietario,
+        viewMode: ViewMode.espectador,
+      });
+      fixture.detectChanges();
+    });
+
+    it('should not show the cards', () => {
+      expect(component.userService.getCurrentUser?.viewMode).toBe(
+        ViewMode.espectador
+      );
+      const cards = fixture.debugElement.query(By.css('a-card'));
+      expect(cards).toBeNull();
+    });
+  });
+
+  describe('When cards are revealed', () => {
+    beforeEach(() => {
+      jest.spyOn(gameService, 'getIsRevealed', 'get').mockReturnValue(true);
+
+      fixture.detectChanges();
+    });
+
+    it('should show the statistics', () => {
+      const statistics = fixture.debugElement.query(By.css('.statistics'));
+      expect(component.isRevealed).toBe(true);
+      expect(statistics).toBeTruthy();
+    });
+
+    it('should show the average', () => {
+      const average = fixture.debugElement.query(By.css('.average'));
+      expect(component.isRevealed).toBe(true);
+      expect(average).toBeTruthy();
+    });
+  });
+
+  describe('When cards are not revealed', () => {
+    beforeEach(() => {
+      jest.spyOn(gameService, 'getIsRevealed', 'get').mockReturnValue(false);
+      fixture.detectChanges();
+    });
+
+    it('should not show the statistics', () => {
+      const statistics = fixture.debugElement.query(By.css('.statistics'));
+      expect(component.isRevealed).toBe(false);
+      expect(statistics).toBeNull();
+    });
+
+    it('should not show the average', () => {
+      const average = fixture.debugElement.query(By.css('.average'));
+      expect(component.isRevealed).toBe(false);
+      expect(average).toBeNull();
+    });
   });
 });
